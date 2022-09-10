63ac7f05ca54d53f1740d4c0863fdfcb3dacffbd
yuyeol <uy23@naver.com>
2022-09-10 18:18:17 +0900

    feature(#1): set recoil

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import { RecoilRoot } from 'recoil';
 import { Global } from '@emotion/react';
 import Layout from 'components/Layout';
 import type { AppProps } from 'next/app';
@@ -5,12 +6,12 @@ import global from 'styles/GlobalStyle';
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <RecoilRoot>
       <Global styles={global} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </RecoilRoot>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
-import Box from 'components/Box';
+import Seo from 'components/Seo';
+import TodoList from 'components/todoList';
 import type { NextPage } from 'next';
+import { useRecoilState } from 'recoil';
+import { todoState } from 'states';
 
 const Home: NextPage = () => {
+  const [todos] = useRecoilState(todoState);
   return (
     <div>
       <Box />
+      <TodoList todos={todos} />
     </div>
   );
 };
